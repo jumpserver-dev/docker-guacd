1fc301794d98eaa8b32130b1eb3ca01e491cde34
Eric <xplzv@126.com>
2025-11-27 16:47:01 +0800

    perf: remove sh files

--- guacd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#docker run --rm --name guacd -p 4822:4822 guacd:main
-docker run --rm --name guacd -p 4822:4822 guacd-trixie:test
--- start.sh
+++
@@ -1,5 +0,0 @@
-cat start.sh 
-docker run \
-  -p 8083:8080 \
-  -v $(pwd)/config:/config \
-  oznu/guacamole
